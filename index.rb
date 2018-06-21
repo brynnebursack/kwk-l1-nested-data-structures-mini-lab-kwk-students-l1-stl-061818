b85d5b51d983c02ef793e45c2f870c31f02d76ce
brynnebursack <brynnebursack@gmail.com>
2018-06-21 15:45:39 +0000

    automatically backed up by learn

--- index.rb
+++ index.rb
@@ -13,6 +13,11 @@ sandwich_request
 
 
 def sandwich_request(ingredients, order) 
+ingredients = [
+    ["rye", "sourdough", "baguette"],
+    ["ham", "salami", "turkey"],
+    ["swiss", "munster", "cheddar"],
+    ["mayo", "mustard", "tabasco"]
  
  
 end
